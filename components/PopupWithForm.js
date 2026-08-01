75316ff1ce04101af5b329e0dfd59a356389f5cf
Joel Alvarez <Joel124@outlook.es>
2026-08-01 01:57:12 -0400

    Normalize popup input list

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -5,7 +5,9 @@ export default class PopupWithForm extends Popup {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".popup__form");
     this._handleFormSubmit = handleFormSubmit;
-    this._inputList = this._popupForm.querySelectorAll(".popup__input");
+    this._inputList = Array.from(
+      this._popupForm.querySelectorAll(".popup__input")
+    );
   }
 
   _getInputValues() {
