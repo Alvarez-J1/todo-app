369ab19b6b760329d55f59137ef9530fbbb15adc
Joel Alvarez <Joel124@outlook.es>
2026-08-13 07:06:17 -0400

    Clarify completed counter filter

--- components/TodoCounter.js
+++ components/TodoCounter.js
@@ -1,7 +1,7 @@
 export default class TodoCounter {
   constructor(todos, selector) {
     this._element = document.querySelector(selector);
-    this._completed = todos.filter((todo) => todo.completed).length;
+    this._completed = todos.filter((todo) => Boolean(todo.completed)).length;
     this._total = todos.length;
     this._updateText();
   }
